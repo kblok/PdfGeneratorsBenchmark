d4f06ab2d0388f7a3b2942f6ec4a82779eb26910
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-03-21 09:11:04 -0300

    Puppeteer Demo

---
+++ Puppeteer/index.js
@@ -0,0 +1,20 @@
+const puppeteer = require("puppeteer");
+const filesToCreate = 10;
+const url = "file:///C:/source/personal/PdfGeneratorsBenchmark/Site/Site.htm";
+const outputFile = __dirname + '/output.pdf';
+
+( async () =>
+{
+    const browser = await puppeteer.launch({ headless: true });
+
+    for(let index = 0; index < filesToCreate; index++)
+    {
+        console.log("Iteration " + index);
+        let page = await browser.newPage();
+        await page.goto(url);
+        await page.pdf({path: outputFile});
+        await page.close();
+    }
+
+    await browser.close();
+})();
